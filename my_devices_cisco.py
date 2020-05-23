9e1ef359901ce12df4417fad3382088e000ec4b3
telecosistem <telecosistem@gmail.com>
2020-05-23 15:38:48 +0200

    New Script using getpass

--- my_devices_cisco.py
+++ my_devices_cisco.py
@@ -1,11 +1,36 @@
-
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Mon May 07 13:33:43 2020
 
 @author: Alfredo B.
 """
+import getpass
 
+username = input('Username: ')
+pwd = getpass.win_getpass()
+
+csrv1_lab = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.255.51',
+    'username': username,
+    'password': pwd,
+}
+csrv2_lab = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.255.52',
+    'username': username,
+    'password': pwd,
+}
+csrv3_lab = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.255.53',
+    'username': username,
+    'password': pwd,
+}
+
+"""
+#Old format
 csrv1_lab = {
     'device_type': 'cisco_ios',
     'ip': '192.168.255.51',
@@ -13,11 +38,7 @@ csrv1_lab = {
     'password': 'python',
 }
 
-csrv2_lab = {
-    'device_type': 'cisco_ios',
-    'ip': '192.168.255.52',
-    'username': 'python',
-    'password': 'python',
-}
+"""
+
 
 
