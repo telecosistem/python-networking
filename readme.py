c39fdf1677818f68629ec0660cc9084ba1eb6c41
telecosistem <telecosistem@gmail.com>
2020-05-23 15:39:32 +0200

    New Readme file

---
+++ readme.py
@@ -0,0 +1,14 @@
+"""
+#The script called: "mtu_find" is useful to retrieve all MTU values from Cisco devices.
+#Currently most of the interfaces should be using JumboFrames(9200).
+
+#Import Libraries:
+    - re (Regular Expressions)
+    - pandas (Split a dictionary in columns)
+    - getpass (to request password dynamically)
+
+# Notes
+    If you are using Pycharm remember "Run > Edit Configurations > Execution > Run with Python Console".
+    Otherwise getpass library is now working.
+
+"""
